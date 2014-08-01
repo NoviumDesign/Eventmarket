6f70848aafeb4dab2e2a52f29a222744616a58b2
Mattias Forsberg <mf@dynamicsource.se>
2014-08-01 13:58:48 +0200

    New Relic + crypto

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['eventmarket'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '4ba7d0e6fff16b8643056cb7e19b70a8bd59d673',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
